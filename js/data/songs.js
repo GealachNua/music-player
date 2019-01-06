c4d39f26547a5ed9287249dd472ca85c84e5159d
Christoph Wiekart <cwiekart@xs4all.nl>
2019-01-06 16:38:49 +0100

    Player was not working. Found fault in songs.js url routing code.

--- js/data/songs.js
+++ js/data/songs.js
@@ -1,27 +1,30 @@
+// file tracking initially failed with path
+// "url": "../../musig/01.mp3"
+
 export const songsList = [{
     "id": "1",
-    "url": "../../music/01.mp3",
+    "url": "./music/01.mp3",
     "time": "04:22",
     "title": "Bill the Weaver/Paddy Lyons'",
     "artist": "Julia Clifford"
   },
   {
     "id": "2",
-    "url": "../../music/gods-plan.mp3",
-    "time": "03:19",
-    "title": "God's Plan",
-    "artist": "Drake"
+    "url": "./music/05.mp3",
+    "time": "03:58",
+    "title": "John J Kimmell's / The Home Brew",
+    "artist": "Julia and John Clifford"
   },
   {
     "id": "3",
-    "url": "../../music/03.mp3",
-    "time": "05:52",
+    "url": "./music/03.mp3",
+    "time": "02:03",
     "title": "The Humours of Lisheen / Art O'Keeffe's",
     "artist": "Julia Clifford and Reg Hall(piano)"
   },
   {
     "id": "4",
-    "url": "../../music/04.mp3",
+    "url": "./music/04.mp3",
     "time": "02:12",
     "title": "The Blue Riband / Merry Girl",
     "artist": "Julia and John Clifford"
